f3982b76fdce5316183107b8633f7d05072bc8be
pallavis60 <pallavi123.xworkz@gmail.com>
2023-04-01 08:47:21 +0530

    Inheritance

---
+++ InheritanceOne/src/com/xworkz/inheritanceone/Telivision.java
@@ -0,0 +1,16 @@
+package com.xworkz.inheritanceone;
+
+public class Telivision {
+       String name;
+       int price;
+       
+       
+       public void giveEntertainment() {
+    	   System.out.println("First method of Telivision");
+       }
+       
+       public void giveInformation() {
+    	   System.out.println("Second method of Telivision");
+       }
+
+}
