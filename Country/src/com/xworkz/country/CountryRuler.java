3ae8d554309ef4dffeea574024e27aa768350fdb
pallavis60 <pallavi123.xworkz@gmail.com>
2023-04-24 10:48:18 +0530

    Encapsulation

---
+++ Country/src/com/xworkz/country/CountryRuler.java
@@ -0,0 +1,82 @@
+package com.xworkz.country;
+
+import com.xworkz.country.countryinfo.Country;
+
+public class CountryRuler {
+	
+	public static void main(String[] args) {
+		Country obj = new Country();
+		
+		
+		obj.setName("India");
+		obj.setnoOfStates(28);
+		obj.setnationalBird("peacock");
+		obj.setnationalAnimal("Tiger");
+		obj.setflag("Tricolour");
+		obj.setnationalFlower("Lotus");
+		obj.setpopulation(1417903777);
+		obj.setnationalAnthem("Jana Gana Mana");
+		obj.setnationalfruit("Mango");
+		obj.setnationalTree("Banyan");
+		obj.setnationalGame("Hockey");
+		obj.setnationalRiver("Ganga");
+		obj.setnationalCurrency("rupee");
+		obj.setlargestState("Rajasthan");
+		obj.setsmallestState("Goa");
+		obj.setcapitalOfIndia("Delhi");
+		obj.setcleanestCityInIndia("Indore");
+		obj.setfastestGrowingCity("Bangalore");
+		obj.setpmOfIndia("NarendraModi");
+		obj.setnativeLanguages(447);
+		obj.setDominion("15 august 1947");
+		obj.setdateFormat("DD- MM-YYYY");
+		obj.setcallingCode(91);
+		obj.setofficialLanguages(22);
+		obj.setpostalNetworks(155531);
+		obj.setunescoHeritagesSites(38);
+		obj.settotalArea("3287263 sqkm");
+		obj.setpresidentOfIndia("DroupadiMurmu");
+		obj.setmilitaryAgeInIndia(18);
+		obj.setpercentOfGdp(2);
+		obj.setisIndiaIsAConscription(false);
+		obj.setbudget("5.94 million");
+		
+		
+		
+		
+		
+	    System.out.println(obj.getName());	
+	    System.out.println(obj.getbudget());	
+     	System.out.println(obj.getcallingCode());
+     	System.out.println(obj.getcapitalOfIndia());	
+        System.out.println(obj.getcleanestCityInIndia());	
+    	System.out.println(obj.getdateFormat());
+    	System.out.println(obj.getDominion());	
+    	System.out.println(obj.getfastestGrowingCity());	
+    	System.out.println(obj.getflag());
+	    System.out.println(obj.getisIndiaIsAConscription());
+	    System.out.println(obj.getlargestState());	
+        System.out.println(obj.getmilitaryAgeInIndia());
+     	System.out.println(obj.getnationalAnimal());
+        System.out.println(obj.getnationalAnthem()); 
+    	System.out.println(obj.getnationalBird());
+    	System.out.println(obj.getnationalCurrency());
+    	System.out.println(obj.getnationalFlower());
+     	System.out.println(obj.getnationalGame());
+    	System.out.println(obj.getnationalRiver());
+	    System.out.println(obj.getnationalTree());
+    	System.out.println(obj.getnativeLanguages());
+     	System.out.println(obj.getpercentOfGdp());
+     	System.out.println(obj.getpmOfIndia());
+    	System.out.println(obj.getnoOfStates());
+    	System.out.println(obj.getofficialLanguages());
+	    System.out.println(obj.getpmOfIndia());	
+	    System.out.println(obj.getpopulation());
+	    System.out.println(obj.getpostalNetworks());
+	    System.out.println(obj.getpresidentOfIndia());
+	    System.out.println(obj.getsmallestState());	
+	    System.out.println(obj.gettotalArea());
+	    System.out.println(obj.getunescoHeritagesSites());
+		}
+
+}
