760993c05bcd26bcdaca0f78e7f7f93f4fe08172
pallavis60 <pallavi123.xworkz@gmail.com>
2023-04-24 22:47:39 +0530

    Dto

---
+++ Shop/src/com/xworkz/shop/ShopRunner.java
@@ -0,0 +1,139 @@
+package com.xworkz.shop;
+
+import com.xworkz.shop.dto.ShopDto;
+
+public class ShopRunner {
+	public static void main(String[] args) {
+		
+		ShopDto obj = new ShopDto("Everly", "Nikitha", "Cloths", 98458352," june 23 2008", "8 am to 9 pm", "Bangalore", 45, "Gowri", 20000, 40000, 50000, true, 2 , 3,  60000, 30000, 20000,  10000,  50000, 500, 50, 15000);
+	
+	  obj.setShopName("Everly");
+	  obj.setOwnerName("Nikitha");
+      obj.setStoreRelated("Cloths");
+	  obj. setShopNo(98458352);
+	  obj. setShopinauguration("june 23 2008");
+	  obj.setShopingTiming("8 am to 9 pm");
+	  obj.setAddress("Bangalore");
+	  obj.setShopBuildingNo(45);
+	  obj.setManagerName("Gowri");
+	  obj.setShopRent(200000);
+	  obj.setTotalExpenture(400000);
+	  obj.setProfitInAMonth(500000);
+	  obj.setHaveBranches(true);
+	  obj.setBranches(2);
+	  obj.setSections(3);
+	  obj.setTotalClothAvailable(60000);
+	  obj.setWomamsColths(20000);
+	  obj.setMenCloths(30000);
+	  obj.setKidCloths(10000);
+	  obj.setMostExpensiveCloth(50000);
+	  obj.setMostLessExpensiveCloth(500);
+	  obj.setnoOfEmployee(50);
+	  obj.setEmployeeMinimumSalary(15000);
+	  obj.setShopColour("Blue");
+	  obj.setFloors(2);
+	  obj.setNoOfRooms(6);
+	  obj.setTotalCustomerPerDay(30);
+	  obj.setSleepWear(500);
+	  obj.setTrialRoom(2);
+	  obj.setBeautyAndPersonalCare(1);
+	  obj.setBusinessModel("BM Fashion");
+	  obj.setSportsAndActiveWear(30);
+	  obj.setLuggageAndTrolleys(50);
+	  obj.setWinterWear(500);
+	  obj.setSummerWear(400);
+	  obj.setWesternwear(600) ;
+	  obj.setBillingRoom(1);
+	  obj.setCounters(5);
+	  obj.setPaymentMethod("CreditCard and DebitCard");
+	  obj.setCustomerFeedback("1 to 5 rating");
+	  obj.setPaymentProcessingFee("5%");
+	  obj.setProfitMargin("60%");
+	  obj.setUpcommingEvents("Ugadi");
+	  obj.setPopularProduct("Jeens");
+	  obj.setEmployeeTraining("customerServiceSikills");
+	  obj.setBrandIdentity("Logo");
+	  obj.setMarketingBudget(500000);
+	  obj.setLocalization("popularRegion");
+	  obj.setpricingStrategy("quality");
+	  obj.setEmployeePerformance("iceAdjust");
+	  obj.setReturnPolicy("attendence");
+	  obj.setSalesAnaliseCheck("only Exchange");
+	  obj.setShopByOccation(5000);
+	  obj.setSalesProjections("Revenue");
+	  obj.setVendurManagment("The No Of Vendurs");
+	  obj.setHaveEmailMarketing(true);
+	  obj.setHaveProdectTesting(true);
+	  obj.setDiscount("5%");
+	  obj.setLossInLastYear(10000);
+	  obj.setMaintenanceCharge(20000);
+	  obj.setBuildingRate(3000000);
+		
+		
+		
+		
+		System.out.println(obj.getShopName());
+		System.out.println(obj.getOwnerName());
+		System.out.println(obj.getStoreRelated());
+		System.out.println(obj.getShopNo());
+		System.out.println(obj.getShopinauguration());
+		System.out.println(obj.getShopingTiming());
+		System.out.println(obj.getAddress());
+		System.out.println(obj.getShopBuildingNo());
+		System.out.println(obj.getManagerName());
+		System.out.println(obj.getShopRent());
+		System.out.println(obj.getTotalExpenture());
+		System.out.println(obj.getProfitInAMonth());
+		System.out.println(obj.getisHaveBranches());
+		System.out.println(obj.getBranches());
+		System.out.println(obj.getSections());
+		System.out.println(obj.getTotalClothAvailable());
+		System.out.println(obj.getWomamsColths());
+		System.out.println(obj.getMenCloths());
+		System.out.println(obj.getKidCloths());
+		System.out.println(obj.getMostExpensiveCloth());
+		System.out.println(obj.getMostLessExpensiveCloth());
+		System.out.println(obj.getnoOfEmployee());
+		System.out.println(obj.getEmployeeMinimumSalary());
+		System.out.println(obj.getShopColour());
+		System.out.println(obj.getFloors());
+		System.out.println(obj.getTotalCustomerPerDay());
+		System.out.println(obj.getNoOfRooms());
+		System.out.println(obj.getSleepWear());
+		System.out.println(obj.getTrialRoom());
+		System.out.println(obj.getBeautyAndPersonalCare());
+		System.out.println(obj.getBusinessModel());
+		System.out.println(obj.getSportsAndActiveWear());
+		System.out.println(obj.getLuggageAndTrolleys());
+		System.out.println(obj.getWinterWear());
+		System.out.println(obj.getSummerWear());
+		System.out.println(obj.getShopName());
+		System.out.println(obj.getWesternwear());
+		System.out.println(obj.getBillingRoom());
+		System.out.println(obj.getCounters());
+		System.out.println(obj.getPaymentProcessingFee());
+		System.out.println(obj.getCustomerFeedback());
+		System.out.println(obj.getPaymentMethod());
+		System.out.println(obj.getProfitMargin());
+		System.out.println(obj.getUpcommingEvents());
+		System.out.println(obj.getPopularProduct());
+		System.out.println(obj.getEmployeeTraining());
+		System.out.println(obj.getBrandIdentity());
+		System.out.println(obj.getLocalization());
+		System.out.println(obj.getMarketingBudget());
+		System.out.println(obj.getpricingStrategy());
+		System.out.println(obj.getEmployeePerformance());
+		System.out.println(obj.getReturnPolicy());
+		System.out.println(obj.getSalesAnaliseCheck());
+		System.out.println(obj.getSalesProjections());
+		System.out.println(obj.getVendurManagment());
+		System.out.println(obj.getisHaveEmailMarketing());
+		System.out.println(obj.getisHaveProdectTesting());
+		System.out.println(obj.getDiscount());
+		System.out.println(obj.getLossInLastYear());
+		System.out.println(obj.getMaintenanceCharge());
+		System.out.println(obj.getBuildingRate());
+		
+		}
+
+}
---
+++ Shop/src/com/xworkz/shop/dto/ShopDto.java
@@ -0,0 +1,831 @@
+package com.xworkz.shop.dto;
+
+public class ShopDto {
+	
+	private String shopName;
+	private String ownerName ;
+	private String storeRelated ;
+	private long shopNo ;
+	private String shopinauguration;
+	private String shopingTiming;
+	private String address ;
+	private int shopBuildingNo;
+	private String managerName ;
+	private int shopRent ;
+	private int totalExpenture ;
+	private int profitInAMonth ;
+	private boolean isHaveBranches ;
+	private int branches;
+	private int sections ;
+	private int totalClothAvailable ;
+	private int womamsColths ;
+	private int menCloths ;
+	private int kidCloths ;
+	private int mostExpensiveCloth ;
+	private int mostLessExpensiveCloth;
+	private int noOfEmployee;
+	private int employeeMinimumSalary;
+	private String shopColour;
+	private int floors ;
+	private int noOfRooms ;
+	private int totalCustomerPerDay;
+	private int sleepWear;
+	private int trialRoom;
+	private int beautyAndPersonalCare;
+	private String businessModel;
+	private int sportsAndActiveWear;
+	private int luggageAndTrolleys;
+	private int winterWear;
+	private int summerWear;
+	private int shopByOccation;
+	private int westernwear;
+	private int billingRoom ;
+	private int counters;
+	private String paymentMethod ;
+	private String customerFeedback ;
+	private String paymentProcessingFee;
+	private String profitMargin ;
+	private String upcommingEvents ;
+	private String popularProduct ;
+	private String employeeTraining;
+	private String brandIdentity ;
+	private String localization ;
+	private int marketingBudget ;
+	private String pricingStrategy ;
+	private String employeePerformance ;
+	private String returnPolicy;
+	private String salesAnaliseCheck ;
+	private String salesProjections;
+	private String vendurManagment ;
+	private boolean isHaveEmailMarketing ;
+	private boolean isHaveProdectTesting ;
+	private String discount;
+	private int lossInLastYear;
+	private int maintenanceCharge ;
+	private int buildingRate ;
+			
+	
+	
+	public ShopDto(String shopName, String ownerName, String storeRelated, long shopNo, String shopinauguration,
+			String shopingTiming, String address, int shopBuildingNo, String managerName, int shopRent,
+			int totalExpenture, int profitInAMonth, boolean isHaveBranches, int branches, int sections,
+			int totalClothAvailable, int womamsColths, int menCloths, int kidCloths, int mostExpensiveCloth,
+			int mostLessExpensiveCloth, int noOfEmployee, int employeeMinimumSalary) {
+		this.shopName = shopName;
+		this.ownerName = ownerName;
+		this.storeRelated = storeRelated;
+		this.shopNo = shopNo;
+		this.shopinauguration = shopinauguration;
+		this.shopingTiming = shopingTiming;
+		this.address = address;
+		this.shopBuildingNo = shopBuildingNo;
+		this.managerName = managerName;
+		this.shopRent = shopRent;
+		this.totalExpenture = totalExpenture;
+		this.profitInAMonth = profitInAMonth;
+		this.isHaveBranches = isHaveBranches;
+		this.branches = branches;
+		this.sections = sections;
+		this.totalClothAvailable = totalClothAvailable;
+		this.womamsColths = womamsColths;
+		this.menCloths = menCloths;
+		this.kidCloths = kidCloths;
+		this.mostExpensiveCloth = mostExpensiveCloth;
+		this.mostLessExpensiveCloth = mostLessExpensiveCloth;
+		this.noOfEmployee = noOfEmployee;
+		this.employeeMinimumSalary = employeeMinimumSalary;
+	}
+
+
+
+	public String getShopName() {
+		return shopName;
+	}
+
+
+
+	public void setShopName(String shopName) {
+		this.shopName = shopName;
+	}
+
+
+
+	public String getOwnerName() {
+		return ownerName;
+	}
+
+
+
+	public void setOwnerName(String ownerName) {
+		this.ownerName = ownerName;
+	}
+
+
+
+	public String getStoreRelated() {
+		return storeRelated;
+	}
+
+
+
+	public void setStoreRelated(String storeRelated) {
+		this.storeRelated = storeRelated;
+	}
+
+
+
+	public long getShopNo() {
+		return shopNo;
+	}
+
+
+
+	public void setShopNo(long shopNo) {
+		this.shopNo = shopNo;
+	}
+
+
+
+	public String getShopinauguration() {
+		return shopinauguration;
+	}
+
+
+
+	public void setShopinauguration(String shopinauguration) {
+		this.shopinauguration = shopinauguration;
+	}
+
+
+
+	public String getShopingTiming() {
+		return shopingTiming;
+	}
+
+
+
+	public void setShopingTiming(String shopingTiming) {
+		this.shopingTiming = shopingTiming;
+	}
+
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+
+	public int getShopBuildingNo() {
+		return shopBuildingNo;
+	}
+
+
+
+	public void setShopBuildingNo(int shopBuildingNo) {
+		this.shopBuildingNo = shopBuildingNo;
+	}
+
+
+
+	public String getManagerName() {
+		return managerName;
+	}
+
+
+
+	public void setManagerName(String managerName) {
+		this.managerName = managerName;
+	}
+
+
+
+	public int getShopRent() {
+		return shopRent;
+	}
+
+
+
+	public void setShopRent(int shopRent) {
+		this.shopRent = shopRent;
+	}
+
+
+
+	public int getTotalExpenture() {
+		return totalExpenture;
+	}
+
+
+
+	public void setTotalExpenture(int totalExpenture) {
+		this.totalExpenture = totalExpenture;
+	}
+
+
+
+	public int getProfitInAMonth() {
+		return profitInAMonth;
+	}
+
+
+
+	public void setProfitInAMonth(int profitInAMonth) {
+		this.profitInAMonth = profitInAMonth;
+	}
+
+
+
+	public boolean getisHaveBranches() {
+		return isHaveBranches;
+	}
+
+
+
+	public void setHaveBranches(boolean isHaveBranches) {
+		this.isHaveBranches = isHaveBranches;
+	}
+
+
+
+	public int getBranches() {
+		return branches;
+	}
+
+
+
+	public void setBranches(int branches) {
+		this.branches = branches;
+	}
+
+
+
+	public int getSections() {
+		return sections;
+	}
+
+
+
+	public void setSections(int sections) {
+		this.sections = sections;
+	}
+
+
+
+	public int getTotalClothAvailable() {
+		return totalClothAvailable;
+	}
+
+
+
+	public void setTotalClothAvailable(int totalClothAvailable) {
+		this.totalClothAvailable = totalClothAvailable;
+	}
+
+
+
+	public int getWomamsColths() {
+		return womamsColths;
+	}
+
+
+
+	public void setWomamsColths(int womamsColths) {
+		this.womamsColths = womamsColths;
+	}
+
+
+
+	public int getMenCloths() {
+		return menCloths;
+	}
+
+
+
+	public void setMenCloths(int menCloths) {
+		this.menCloths = menCloths;
+	}
+
+
+
+	public int getKidCloths() {
+		return kidCloths;
+	}
+
+
+
+	public void setKidCloths(int kidCloths) {
+		this.kidCloths = kidCloths;
+	}
+
+
+
+	public int getMostExpensiveCloth() {
+		return mostExpensiveCloth;
+	}
+
+
+
+	public void setMostExpensiveCloth(int mostExpensiveCloth) {
+		this.mostExpensiveCloth = mostExpensiveCloth;
+	}
+
+
+
+	public int getMostLessExpensiveCloth() {
+		return mostLessExpensiveCloth;
+	}
+
+
+
+	public void setMostLessExpensiveCloth(int mostLessExpensiveCloth) {
+		this.mostLessExpensiveCloth = mostLessExpensiveCloth;
+	}
+
+
+
+	public int getnoOfEmployee() {
+		return noOfEmployee;
+	}
+
+
+
+	public void setnoOfEmployee(int noOfEmployee) {
+		this.noOfEmployee = noOfEmployee;
+	}
+
+
+
+	public int getEmployeeMinimumSalary() {
+		return employeeMinimumSalary;
+	}
+
+
+
+	public void setEmployeeMinimumSalary(int employeeMinimumSalary) {
+		this.employeeMinimumSalary = employeeMinimumSalary;
+	}
+
+
+
+	public String getShopColour() {
+		return shopColour;
+	}
+
+
+
+	public void setShopColour(String shopColour) {
+		this.shopColour = shopColour;
+	}
+
+
+
+	public int getFloors() {
+		return floors;
+	}
+
+
+
+	public void setFloors(int floors) {
+		this.floors = floors;
+	}
+
+
+
+	public int getNoOfRooms() {
+		return noOfRooms;
+	}
+
+
+
+	public void setNoOfRooms(int noOfRooms) {
+		this.noOfRooms = noOfRooms;
+	}
+
+
+
+	public int getTotalCustomerPerDay() {
+		return totalCustomerPerDay;
+	}
+
+
+
+	public void setTotalCustomerPerDay(int totalCustomerPerDay) {
+		this.totalCustomerPerDay = totalCustomerPerDay;
+	}
+
+
+
+	public int getSleepWear() {
+		return sleepWear;
+	}
+
+
+
+	public void setSleepWear(int sleepWear) {
+		this.sleepWear = sleepWear;
+	}
+
+
+
+	public int getTrialRoom() {
+		return trialRoom;
+	}
+
+
+
+	public void setTrialRoom(int trialRoom) {
+		this.trialRoom = trialRoom;
+	}
+
+
+
+	public int getBeautyAndPersonalCare() {
+		return beautyAndPersonalCare;
+	}
+
+
+
+	public void setBeautyAndPersonalCare(int beautyAndPersonalCare) {
+		this.beautyAndPersonalCare = beautyAndPersonalCare;
+	}
+
+
+
+	public String getBusinessModel() {
+		return businessModel;
+	}
+
+
+
+	public void setBusinessModel(String businessModel) {
+		this.businessModel = businessModel;
+	}
+
+
+
+	public int getSportsAndActiveWear() {
+		return sportsAndActiveWear;
+	}
+
+
+
+	public void setSportsAndActiveWear(int sportsAndActiveWear) {
+		this.sportsAndActiveWear = sportsAndActiveWear;
+	}
+
+
+
+	public int getLuggageAndTrolleys() {
+		return luggageAndTrolleys;
+	}
+
+
+
+	public void setLuggageAndTrolleys(int luggageAndTrolleys) {
+		this.luggageAndTrolleys = luggageAndTrolleys;
+	}
+
+
+
+	public int getWinterWear() {
+		return winterWear;
+	}
+
+
+
+	public void setWinterWear(int winterWear) {
+		this.winterWear = winterWear;
+	}
+
+
+
+	public int getSummerWear() {
+		return summerWear;
+	}
+
+
+
+	public void setSummerWear(int summerWear) {
+		this.summerWear = summerWear;
+	}
+
+
+
+	public int getShopByOccation() {
+		return shopByOccation;
+	}
+
+
+
+	public void setShopByOccation(int shopByOccation) {
+		this.shopByOccation = shopByOccation;
+	}
+
+
+
+	public int getWesternwear() {
+		return westernwear;
+	}
+
+
+
+	public void setWesternwear(int westernwear) {
+		this.westernwear = westernwear;
+	}
+
+
+
+	public int getBillingRoom() {
+		return billingRoom;
+	}
+
+
+
+	public void setBillingRoom(int billingRoom) {
+		this.billingRoom = billingRoom;
+	}
+
+
+
+	public int getCounters() {
+		return counters;
+	}
+
+
+
+	public void setCounters(int counters) {
+		this.counters = counters;
+	}
+
+
+
+	public String getPaymentMethod() {
+		return paymentMethod;
+	}
+
+
+
+	public void setPaymentMethod(String paymentMethod) {
+		this.paymentMethod = paymentMethod;
+	}
+
+
+
+	public String getCustomerFeedback() {
+		return customerFeedback;
+	}
+
+
+
+	public void setCustomerFeedback(String customerFeedback) {
+		this.customerFeedback = customerFeedback;
+	}
+
+
+
+	public String getPaymentProcessingFee() {
+		return paymentProcessingFee;
+	}
+
+
+
+	public void setPaymentProcessingFee(String paymentProcessingFee) {
+		this.paymentProcessingFee = paymentProcessingFee;
+	}
+
+
+
+	public String getProfitMargin() {
+		return profitMargin;
+	}
+
+
+
+	public void setProfitMargin(String profitMargin) {
+		this.profitMargin = profitMargin;
+	}
+
+
+
+	public String getUpcommingEvents() {
+		return upcommingEvents;
+	}
+
+
+
+	public void setUpcommingEvents(String upcommingEvents) {
+		this.upcommingEvents = upcommingEvents;
+	}
+
+
+
+	public String getPopularProduct() {
+		return popularProduct;
+	}
+
+
+
+	public void setPopularProduct(String popularProduct) {
+		this.popularProduct = popularProduct;
+	}
+
+
+
+	public String getEmployeeTraining() {
+		return employeeTraining;
+	}
+
+
+
+	public void setEmployeeTraining(String employeeTraining) {
+		this.employeeTraining = employeeTraining;
+	}
+
+
+
+	public String getBrandIdentity() {
+		return brandIdentity;
+	}
+
+
+
+	public void setBrandIdentity(String brandIdentity) {
+		this.brandIdentity = brandIdentity;
+	}
+
+
+	public String getLocalization() {
+		return localization;
+	}
+
+
+
+	public void setLocalization(String localization) {
+		this.localization = localization;
+	}
+
+
+
+	public int getMarketingBudget() {
+		return marketingBudget;
+	}
+
+
+
+	public void setMarketingBudget(int marketingBudget) {
+		this.marketingBudget = marketingBudget;
+	}
+
+
+
+	public String getpricingStrategy() {
+		return pricingStrategy;
+	}
+
+
+
+	public void setpricingStrategy(String pricingStrategy) {
+		this.pricingStrategy = pricingStrategy;
+	}
+
+
+
+	public String getEmployeePerformance() {
+		return employeePerformance;
+	}
+
+
+
+	public void setEmployeePerformance(String employeePerformance) {
+		this.employeePerformance = employeePerformance;
+	}
+
+
+
+	public String getReturnPolicy() {
+		return returnPolicy;
+	}
+
+
+
+	public void setReturnPolicy(String returnPolicy) {
+		this.returnPolicy = returnPolicy;
+	}
+
+
+
+	public String getSalesAnaliseCheck() {
+		return salesAnaliseCheck;
+	}
+
+
+
+	public void setSalesAnaliseCheck(String salesAnaliseCheck) {
+		this.salesAnaliseCheck = salesAnaliseCheck;
+	}
+
+
+
+	public String getSalesProjections() {
+		return salesProjections;
+	}
+
+
+
+	public void setSalesProjections(String salesProjections) {
+		this.salesProjections = salesProjections;
+	}
+
+
+	public String getVendurManagment() {
+		return vendurManagment;
+	}
+
+
+
+	public void setVendurManagment(String vendurManagment) {
+		this.vendurManagment = vendurManagment;
+	}
+
+
+
+	public boolean getisHaveEmailMarketing() {
+		return isHaveEmailMarketing;
+	}
+
+
+
+	public void setHaveEmailMarketing(boolean isHaveEmailMarketing) {
+		this.isHaveEmailMarketing = isHaveEmailMarketing;
+	}
+
+
+
+	public boolean getisHaveProdectTesting() {
+		return isHaveProdectTesting;
+	}
+
+
+
+	public void setHaveProdectTesting(boolean isHaveProdectTesting) {
+		this.isHaveProdectTesting = isHaveProdectTesting;
+	}
+	
+	
+	public String getDiscount() {
+		return discount;
+	}
+
+
+
+	public void setDiscount(String discount) {
+		this.discount = discount;
+	}
+
+
+
+	public int getLossInLastYear() {
+		return lossInLastYear;
+	}
+
+
+
+	public void setLossInLastYear(int lossInLastYear) {
+		this.lossInLastYear = lossInLastYear;
+	}
+
+
+
+	public int getMaintenanceCharge() {
+		return maintenanceCharge;
+	}
+
+
+
+	public void setMaintenanceCharge(int maintenanceCharge) {
+		this.maintenanceCharge = maintenanceCharge;
+	}
+	
+	
+	
+	public int getBuildingRate() {
+		return buildingRate;
+	}
+
+
+
+	public void setBuildingRate(int buildingRate) {
+		this.buildingRate = buildingRate;
+	}
+
+	
+	
+
+}
